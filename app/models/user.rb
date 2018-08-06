20c0d4922f816c14ac993c9b9cf7a3bf78f02c51
koombea <koombea@koombeas-MacBook-Pro.local>
2018-08-06 17:30:20 -0500

    Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  validation :first_name, presence: true,
+    :last_name, presence: true,
+    :address, presence: true,
+    :password, presence: true,
+    :email, presence: true,
+    :city, presence: true,
+    :state, presence: true,
+    :cellphone, presence: true,
+    :phone, presence: true,
+    :company, presence: true,
 end
