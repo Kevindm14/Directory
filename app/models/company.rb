9f2fad3ba6aefebf323c57376052de9a76320b61
koombea <koombea@koombeas-MacBook-Pro.local>
2018-08-06 17:43:32 -0500

    Model-company-validation

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,9 @@
 class Company < ApplicationRecord
+   validation :name, presence: true,
+    :address, presence: true,
+    :city, presence: true,
+    :state, presence: true,
+    :commercial_activity, presence: true,
+    :cellphone, presence: true,
+    :phone, presence: true,
 end
