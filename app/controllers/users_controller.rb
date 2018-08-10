dae32664a0375f3a23594eb2d9fc7591c19afe72
Kevin <kevin.diaz@koombea.com>
2018-08-10 11:22:57 -0500

    View user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,20 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, except: [:index, :new, :create]
+  
   def index
     @users = User.all
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
-
   def new
     @user = User.new
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def create
@@ -22,6 +27,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def set_user
+   @user = User.find(params[:id]) 
+  end
 
   def user_params
     params.require(:user).permit(
