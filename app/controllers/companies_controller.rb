443b3e193306d3f14a90eb57737bcf6305ab54bb
Kevin <kevin.diaz@koombea.com>
2018-08-10 14:55:39 -0500

    Company new view

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -7,4 +7,31 @@ class CompaniesController < ApplicationController
   def show
     @company = Company.find(params[:id])
   end
+
+  def new
+    @company = Company.new
+  end
+
+  def create 
+    @company = Company.new(company_params)
+    if @company.save
+      redirect_to @company
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(
+      :name,
+      :address,
+      :city,
+      :state,
+      :commercial_activity,
+      :cellphone,
+      :phone
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  resources :companies, only: [:index, :show]
+  resources :companies, only: [:index, :show, :new, :create]
   resources :users
   devise_for :users
   root to: "home#index"
