a7ba07655f8729a3180502361d0155f16bc97970
Kevin <kevin.diaz@koombea.com>
2018-08-10 15:57:04 -0500

    Edit view company

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,15 +1,19 @@
 class CompaniesController < ApplicationController
-  
+  before_action :set_company, except: [:index, :new, :create]
   def index
     @companies = Company.all
   end
 
-  def show
-    @company = Company.find(params[:id])
-  end
-
   def new
     @company = Company.new
+  end
+
+  def update
+   if @company.update(company_params)
+      redirect_to @company
+    else
+      render 'edit'
+    end
   end
 
   def create 
@@ -22,6 +26,10 @@ class CompaniesController < ApplicationController
   end
 
   private
+
+  def set_company
+    @company = Company.find(params[:id])
+  end
 
   def company_params
     params.require(:company).permit(
