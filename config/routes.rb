ca04ce21a955c4aeede0f4b05f125b2f6d7005a2
Kevin <kevin.diaz@koombea.com>
2018-08-15 17:34:56 -0500

    Import contacts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  
+  match "/contacts/:importer/callback" => "google#index", :via => [:get]
+  match "/oauth2callback" => "google#contact", :via => [:get]
   resources :companies
   resources :users
   devise_for :users, path: 'accounts', controllers: {omniauth_callbacks: "omniauth_callbacks"}
